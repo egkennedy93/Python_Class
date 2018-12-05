1341aa3bf430026f5b4848e8d0c8e1aa721a9464
egkennedy93 <egkennedy93@gmail.com>
2018-12-04 22:28:49 -0500

    Add files via upload

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -8,19 +8,21 @@ import random
 
 
 def player_input():
-    while True:
+    ready = True
+    while ready:
         play1_input = input("Player 1: Do you want to be X or O?\n").upper()
         if play1_input == "X" or play1_input == "O":
             print(f"Player 1 selected: \"{play1_input}\"")
             print(choose_first())
-            return False
+            return play1_input
         else:
             print("Incorrect option. Please enter \"X\" or \"O\"\n ")
-            return True
+            ready = True
 
 
 def place_marker(board, marker, position):
     tmp_list = board
+    tmp_list.pop(position)
     tmp_list.insert(position, marker)
     return
 
@@ -41,10 +43,15 @@ def game_board(board):
 
 
 def win_check(board, mark):
-    if board[1:3] == mark or board[4:6] == mark or board[7:9] == mark:
+    x_win = ["X", "X", "X"]
+    o_win = ["O", "O", "O"]
+    if board[1:4] == x_win or board[4:7] == x_win or board[7:10] == x_win:
         print("{} wins!".format(mark))
         return True
-    elif board[1] == mark  and board[5] == mark and board[9] == mark:
+    if board[1:4] == o_win or board[4:7] == o_win or board[7:10] == o_win:
+        print("{} wins!".format(mark))
+        return True
+    elif board[1] == mark and board[5] == mark and board[9] == mark:
         print("{} wins!".format(mark))
         return True
     elif board[3] == mark and board[5] == mark and board[7] == mark:
@@ -73,16 +80,19 @@ def space_check(board, position):
 
 def full_board_check(board):
     for i in board:
-        if i == ' ':
+        if i == ' ' or i == "#":
             return False
         else:
             return True
 
 
 def player_choice(board):
+    choice = input("Choose your next position: (1-9)")
+    if space_check(board, int(choice)) is True:
+        return int(choice)
 
-
-
+    else:
+        return "That position is already taken"
 
 
 def replay():
@@ -98,43 +108,49 @@ def replay():
 def choose_first():
     choose = random.randint(1,10)
     if choose % 2 == 0:
-        return "Player one has been selected to go first!"
+        return "Player one has been selected to go first! \n"
     else:
-        return "Player two has been selected to go first"
+        return "Player two has been selected to go first! \n"
 
 
-#This is function selects what letter Player 1 wants to be, and if he's ready to start
-#player_input()
-#test_board = ['#','X','O','X','O','X','O','X','O','X']
-#place_marker(test_board, "%", 7)
-
-
-print("Welcome to Tic Tac Toe!\n")
-player_input()
-prompt_play = input("Are you ready to play? Enter Yes or No.\n").upper()
-board_list = ["#", ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-
-if prompt_play == "YES":
-    while True:
-        game_board(board_list)
-        win_check(board_list,"X")
-elif prompt_play == "NO":
-    print("Exiting...")
-    exit()
-
-
-
-
-####### Prompt player if they are ready to play y/n ####################
-
-### |  | ###
-### |  | ###
-### |  | ###
-#----------#
-### |  | ###
-### |  | ###
-### |  | ###
-#----------#
-### |  | ###
-### |  | ###
-### |  | ###
+while True:
+    print("Welcome to Tic Tac Toe!\n")
+    board_list = ["#", ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    start_mark = player_input()
+    prompt_play = input("Are you ready to play? Enter Yes or No.\n").upper()
+    turn = 1
+    if prompt_play == "YES":
+        while full_board_check(board_list) is False:
+            if turn % 2 != 0:
+                play1_mark = start_mark
+                game_board(board_list)
+                selection = player_choice(board_list)
+                place_marker(board_list, play1_mark, selection)
+                print("\n" * 100)
+                turn = turn + 1
+                if win_check(board_list, play1_mark):
+                    game_board(board_list)
+                    if replay():
+                        break
+                    else:
+                        quit()
+                else:
+                    continue
+            elif turn % 2 == 0:
+                if start_mark == "X":
+                    play2_mark = "O"
+                else:
+                    play2_mark = "X"
+                game_board(board_list)
+                selection = player_choice(board_list)
+                place_marker(board_list, play2_mark, selection)
+                print("\n" * 100)
+                turn = turn + 1
+                if win_check(board_list, play2_mark):
+                    game_board(board_list)
+                    if replay():
+                        break
+                    else:
+                        quit()
+                else:
+                    continue
